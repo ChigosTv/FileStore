62b6c19b8c5056148354998c1a24e20aa4a45f22
ChigosTv <139185409+ChigosTv@users.noreply.github.com>
2024-07-03 07:59:34 +0100

    Chigos1 start.py

--- plugins/start.py
+++ plugins/start.py
@@ -117,8 +117,8 @@ REPLY_ERROR = """<code>Use this command as a reply to any telegram message with 
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="Join Channel 1", url=client.invitelink),
-            InlineKeyboardButton(text="Join Channel 2", url=client.invitelink2),
+            InlineKeyboardButton(text="Join Channel1", url=client.invitelink1),
+            InlineKeyboardButton(text="Join Channel2", url=client.invitelink2),
         ]
     ]
     try:
